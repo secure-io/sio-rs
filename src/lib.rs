6003f8c5e2071de81e57aeb4057c845bfc5f6a1a
Andreas Auernhammer <aead@mail.de>
2019-06-25 15:59:46 +0200

    make Aad generic over the algorithm
    This commit makes the associated data type Aad
    generic over the AEAD algorithm. The purpose of
    this change is to ensure that we can check within
    the type system whether a particular Aad variable
    can be used for an `EncWriter`  `DecWriter`.
    
    `Aad` implements `Copy` and `Clone` explicitly since
    deriving them would also require the type parameter (`Algorithm`)
    to be e.g. `Copy`.
    See https://doc.rust-lang.org/std/marker/trait.Copy.html for
    more details.
    
    Further, Aad now implements `From<&[u8]` such that
    using it becomes easier by just calling `into()`.

--- src/lib.rs
+++ src/lib.rs
@@ -17,15 +17,17 @@ mod tests {
 
     use super::ring::AES_256_GCM;
     use super::*;
-    use std::io::Write;
+    use std::io::{sink, Write};
 
     #[test]
     fn test_it2() {
         let key: Key<ring::AES_256_GCM> = Key::new([0; AES_256_GCM::KEY_LEN]);
 
         let nonce = Nonce::new([0; Nonce::<AES_256_GCM>::SIZE]);
+        let aad = "Some authenticated but not encrypted data"
+            .as_bytes()
+            .into();
 
-        let aad = Aad::from("Some authenticated but not encrypted data".as_bytes());
         let plaintext = "".as_bytes();
         let mut ciphertext: Vec<u8> = Vec::default(); // Store the ciphertext in memory.
         let mut writer = EncWriter::new(&mut ciphertext, &key, nonce, aad);
@@ -33,7 +35,15 @@ mod tests {
         writer.write_all(plaintext).unwrap();
         writer.close().unwrap(); // Complete the encryption process explicitly.
 
-        println!("{:?}", ciphertext);
+        let mut writer = DecWriter::new(
+            sink(),
+            &key,
+            Nonce::new([0; Nonce::<AES_256_GCM>::SIZE]),
+            aad,
+        );
+
+        writer.write_all(ciphertext.as_slice()).unwrap();
+        writer.close().unwrap(); // Complete the encryption process explicitly.
     }
 
 }
